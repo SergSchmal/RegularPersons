9ac3e097395a9d04469bb92e2e260c0397966473
Sergej Schmal <s.shmall@hotmail.com>
2018-06-09 08:44:26 +0200

    Add project files.

---
+++ ListDataTemplate/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListDataTemplate
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            CreateList();
+        }
+
+        public ObservableCollection<RegularPerson> RegularPersons { get; set; }
+
+        private void CreateList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
---
+++ ListDataTemplate/RegularPerson.cs
@@ -0,0 +1,16 @@
+﻿namespace ListDataTemplate
+{
+    public class RegularPerson
+    {
+        public int CardColor { get; set; }
+
+        public int CardNumber { get; set; }
+
+        public int TypePerson { get; set; }
+
+        public string Room { get; set; }
+
+        public string WaitTime { get; set; }
+
+    }
+}
